b21c5f64ea2d7a5a3eeaca454970ee945b787456
Gsingh225 <100107232+Gsingh225@users.noreply.github.com>
2023-03-05 01:26:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-#hola
+from flask import Flask, request, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'Get':
+        return render_template('register.html')
+
+
+if __name__ == '__main__':
+    app.run()
