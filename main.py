fe58e78825895972a95debe79b1c946a617f53e5
Gsingh225 <gsingh5864@bths.edu>
2023-03-05 14:26:23 +0000

    typo

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ class User(db.Model):
     password = db.Column(db.String(40), nullable=False)
 
 with app.app_context():
-    db.drop_all
-    db.create_all
+    db.drop_all()
+    db.create_all()
 
 
 @app.route('/')
@@ -30,6 +30,24 @@ def register():
         db.session.add(user)
         db.session.commit()
         return f'Your username is "{username}", and password is "{password}. Account created successfully. Please return to the homepage to login!"'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html', err="")
+    elif request.method == 'POST':
+        username = request.form['user']
+        password = request.form['pas']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user is not None:
+            return redirect(url_for('account'))
+        else:
+            return render_template('login.html', err='Invalid Username or Password')
+
+
+@app.route('/account')
+def account():
+    return 'LOGIN WAS A SUCCESS'
 
 
 if __name__ == '__main__':
